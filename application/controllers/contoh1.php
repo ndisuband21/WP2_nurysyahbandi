369f76a2a121cd85d2cfd27088d02ce9d4dc034e
ndisuband21 <ndisuband21@gmail.com>
2022-09-19 20:28:53 +0700

    contoh1

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -1,9 +1,9 @@
 <?php
-class contoh extends CI_Controller
+class contoh1 extends CI_Controller
 {
     public function index()
     {
         echo "<h1> Perkenalkan </h1>";
-        echo " Nama saya Nur Syahbandi tinggal di sungai jawi hobi saya berenang"
+        echo " Nama saya Nur Syahbandi tinggal di sungai jawi hobi saya berenang";
     }
 }
