391fcb4aa7e7427b85d0beb7be2a1be552127406
ndisuband21 <ndisuband21@gmail.com>
2022-10-03 19:02:31 +0700

    pertemuan 3 css

--- application/views/v_header.php
+++ application/views/v_header.php
@@ -9,18 +9,18 @@
 <body>
     <div id="wrapper">
         <header>
-        <hgroup>
+            <hgroup>
             <h1>RentalBuku.net</h1>
             <h3>Membuat Template Sederhana dengan 
-CodeIgniter</h3>
-        </hgroup>
-        <nav>
-            <ul>
-                <li><a href="<?php echo 
-base_url().'index.php/web' ?>">Home</a></li>
-                <li><a href="<?php echo 
-base_url().'index.php/web/about' ?>">About</a></li>
-            </ul>
-        </nav>
+            CodeIgniter</h3>
+            </hgroup>
+            <nav>
+                <ul>
+                    <li><a href="<?php echo 
+                    base_url().'index.php/web' ?>">Home</a></li>
+                    <li><a href="<?php echo 
+                    base_url().'index.php/web/about' ?>">About</a></li>
+                </ul>
+            </nav>
         <div class="clear"></div>
     </header>
