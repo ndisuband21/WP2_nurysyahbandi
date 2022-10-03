6620faac3cd1af2531a82e1c7f65932f696f72f0
ndisuband21 <ndisuband21@gmail.com>
2022-10-03 18:57:44 +0700

    pertemuan 3 css

--- application/views/v_header.php
+++ application/views/v_header.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="utf-8">
     <title>Web Prog II | Merancang Template sederhana dengan 
-codeigniter</title>
+    codeigniter</title>
     <link rel="stylesheet" type="text/css" href="<?php echo 
-base_url() ?>assets/css/stylebuku.css">
+    base_url() ?>assets/css/stylebuku.css">
 </head>
 <body>
     <div id="wrapper">
